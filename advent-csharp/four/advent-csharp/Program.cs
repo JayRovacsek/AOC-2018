08971b69e5f97e3b06a25bb6ea45d3646c1c7ac7
Jay Rovacsek <jayrovacsek@gmail.com>
2018-12-06 21:13:26 +1100

    remove old code

--- advent-csharp/four/advent-csharp/Program.cs
+++ advent-csharp/four/advent-csharp/Program.cs
@@ -19,17 +19,6 @@ namespace advent_csharp
 
             Console.WriteLine($"El' Sleepy: {sleepyHead.Id}, Time asleep: {sleepyHead.TimeAsleep}, Answer = {sleepyHead.Id*mostAsleepMinute}");
 
-
-
-
-            //var fabricSquares = programLogic.CountDoubleClaims(matrix);
-
-            //Console.WriteLine($"Fabric tiles count used by two or more claims is: {fabricSquares}");
-
-            //var claimNotOverlapping = programLogic.DetermineNonOverlapping(claims);
-
-            //Console.WriteLine($"Fabric claim not overlapping with any other claim is: {claimNotOverlapping}");
-            //Console.WriteLine("Press any key to end");
             Console.ReadKey();
         }
     }
